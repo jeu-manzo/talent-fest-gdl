6683a56a53be578c04d2ab1ed9ead68d937a3617
Nathalie <nathaliecortz@hotmail.com>
2019-08-06 15:06:55 -0500

    Add MapComponent

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,4 +1,55 @@
-import React from 'react';
+
+
+import React from "react";
+// import Button from 'react-bootstrap/Button';
+
+class Filters extends React.Component {
+  render() {
+    return (
+      <div className="container" style={{ width: "100%", marginTop: "10px", backgroundColor: "#00758f",textAlign:"center", fontSize:"20px", fontFamily:'Palatino Linotype', color:"white" }} >
+        <div className="row">
+          <div className="col-3">
+            <label htmlFor="ratingFilter">Calificacion:</label>{" "}
+            <select
+              className="custom-select"
+              name="ratingFilter"
+              value={this.props.filters.ratingFilter}
+              onChange={this.props.setFilter}
+            >
+              <option value={-1}>Mostrar todo</option>
+              <option value={1}>1 o más</option>
+              <option value={2}>2 o más</option>
+              <option value={3}>3 o más</option>
+              <option value={4}>4 o más</option>
+              <option value={5}>5</option>
+            </select>
+          </div>
+          <div className="col-3">
+            <label htmlFor="ratingFilter">Nivel:</label>{" "}
+            <select
+              className="custom-select"
+              name="ratingFilter"
+              value={this.props.filters.lvlFilter}
+              onChange={this.props.setFilter}
+            >
+              <option value={-1}>Primaria</option>
+              <option value={1}>Preescolar</option>
+              <option value={2}>Secundaria</option>
+              <option value={3}>Preparatoria</option>
+              <option value={4}>Universidad</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Filters;
+
+
+
+/*import React from 'react';
 import '../styles/Filters.css';
 
 
@@ -53,4 +104,4 @@ class Filters extends React.Component{
     }
 }
 
-export default Filters;
+export default Filters;*/
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,135 @@
-import React from 'react';
+
+import React from "react";
+import MapComponent from "./MapComponent";
+import Filters from "./Filters";
+import NavBar from './Nav-bar';
+import '../styles/Home.css';
+import { Card, Button } from 'react-bootstrap';
+// import { Link } from "react-router-dom";
+// import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+
+class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loadedPlaces: [],
+      filteredPlaces: [],
+      filters: {
+        // -1 es el estado inicial
+        lvlFilter: -1,
+        ratingFilter: -1
+      },
+    };
+  }
+
+  setLoadedPlaces = loadedPlaces => {
+    this.setState({ loadedPlaces });
+  };
+
+  setFilter = e => {
+    const { value, name } = e.target;
+
+    let newFilters = { [name]: Number(value) };
+    if (name === "lvlFilter") {
+      newFilters.ratingFilter = this.state.filters.ratingFilter;
+    } else {
+      newFilters.lvlFilter = this.state.filters.lvlFilter;
+    }
+    this.setState({ filters: newFilters }, this.setFilteredPlaces);
+  };
+
+  setFilteredPlaces = () => {
+    // traer solo los que cumplen las condiciones
+    const filteredPlaces = this.state.loadedPlaces.filter(e => {
+      const lvlFilter =
+        this.state.filters.lvlFilter > -1
+          ? e.lvl_level <= this.state.filters.lvlFilter
+          : true;
+      const ratingFilter =
+        this.state.filters.ratingFilter > -1
+          ? e.rating >= this.state.filters.ratingFilter
+          : true;
+          console.log('rating filter')
+      return lvlFilter && ratingFilter;
+    });
+    this.setState({ filteredPlaces });
+  };
+
+  getGroupedPlaces = () => {
+    let placesInGroupsOfTwo = [];
+
+    this.state.filteredPlaces.forEach((place, i) => {
+      if (i % 3 === 0) {
+        placesInGroupsOfTwo.push([place]);
+      } else {
+        let lastArray = placesInGroupsOfTwo[placesInGroupsOfTwo.length - 1];
+        lastArray.push(place);
+      }
+    });
+    return placesInGroupsOfTwo;
+  };
+
+  render() {
+    window.filteredPlaces = this.state.filteredPlaces;
+    return (
+      <div>
+          <NavBar/>
+        <Filters filters={this.state.filters} setFilter={this.setFilter} />
+
+        <MapComponent
+            // mainLocation={this.props.mainLocation}
+            filteredPlaces={this.state.filteredPlaces}
+            loadedPlaces={this.state.loadedPlaces}
+            setLoadedPlaces={this.setLoadedPlaces}
+            setFilteredPlaces={this.setFilteredPlaces}
+        />
+
+        <div className="container" style={{ marginTop: "20px" }}>
+          {this.getGroupedPlaces().map((places, i) => (
+            <div className="row" key={`places${i}`}>
+              {places.map((place, i2) => (
+                <div
+                  className="col-md card"
+                  key={`place${i2}`}
+                  style={{ margin: "5px", paddingTop: "5px" }}
+                >
+                    <div className="card-body">
+                    <h6 className="card-title" style={{'fontSize': '20px'}}>{place.name}</h6>
+                    <p>{place.vicinity}</p>
+                  </div>
+                  <Button variant="primary">Ver más</Button>
+                  {place.photos && place.photos.length > 0 ? (
+                    <img
+                      className="card-img-top"
+                      src={place.photos[0].getUrl()}
+                      alt={place.name}
+                     
+                    />
+                  ) : (
+                    <img
+                      className="card-img-top"
+                      src='http://louispasteur.edu.mx/images/2018/02/02/primaria.jpg'
+                      alt={place.name}
+                    
+
+                    />
+                  )}
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
+
+
+
+
+/*import React from 'react';
 import Filters from './Filters';
 import PreliminarCard from './Preliminar-card';
 import NavBar from './Nav-bar';
@@ -23,3 +154,4 @@ class Home extends React.Component{
 }
 
 export default Home;
+*/
