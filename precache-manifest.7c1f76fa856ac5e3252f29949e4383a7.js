5b3512377b4b0e6e5dbaef6f2bf080d96a0b8565
unknown <yolanda.ribas@hotmail.com>
2019-08-06 11:00:28 -0500

    Updates

---
+++ precache-manifest.7c1f76fa856ac5e3252f29949e4383a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75e2bc6337128c09f20b684f575f4efb",
+    "url": "/talent-fest-gdl/index.html"
+  },
+  {
+    "revision": "1abe08d7d3f60bbcec0f",
+    "url": "/talent-fest-gdl/static/css/main.943430c2.chunk.css"
+  },
+  {
+    "revision": "598d1abf26432d57d69d",
+    "url": "/talent-fest-gdl/static/js/2.12da9320.chunk.js"
+  },
+  {
+    "revision": "1abe08d7d3f60bbcec0f",
+    "url": "/talent-fest-gdl/static/js/main.38a30b46.chunk.js"
+  },
+  {
+    "revision": "2f76e7f0a03c673e3d1d",
+    "url": "/talent-fest-gdl/static/js/runtime~main.d19191fd.js"
+  }
+]);
